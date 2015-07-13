2c1ac77c7e9d395c257c3d95df53306618fcefda
Richard Hennigan <rhennigan@gmail.com>
2015-07-13 10:04:15 -0400

    update

---
+++ ImageProcessingWSC2015.m
@@ -0,0 +1,22 @@
+(* Mathematica Package         *)
+(* Created by IntelliJ IDEA    *)
+
+(* :Title: ImageProcessingWSC2015     *)
+(* :Context: ImageProcessingWSC2015`  *)
+(* :Author: rhennigan            *)
+(* :Date: 7/13/2015              *)
+
+(* :Package Version: 1.0       *)
+(* :Mathematica Version:       *)
+(* :Copyright: (c) 2015 rhennigan *)
+(* :Keywords:                  *)
+(* :Discussion:                *)
+
+BeginPackage["ImageProcessingWSC2015`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"] (* Begin Private Context *)
+
+End[] (* End Private Context *)
+
+EndPackage[]
